0df08ebde63cfed17ded8bd7ffac1bcf13a30f9a
Christopher Groskopf <staringmonkey@gmail.com>
2011-05-11 21:07:12 -0500

    Tweaked setup.py.

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 
 setup(
     name = "django-boundaryservice",
-    version = "0.1.2",
+    version = "0.1.3",
     description = "A reusable system for aggregating and providing API access to regional boundary data.",
-    #long_description = open('README.rst').read(),
+    long_description = 'See `django-boundaryservice <https://github.com/newsapps/django-boundaryservice>`_ on Github for more information.',
     author='Christopher Groskopf',
     author_email='staringmonkey@gmail.com',
     url='http://blog.apps.chicagotribune.com/',
