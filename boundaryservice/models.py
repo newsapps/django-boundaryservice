838c111f40e1632f7cfe233a3f8a8c957d17db17
Christopher Groskopf <staringmonkey@gmail.com>
2011-05-14 21:08:08 -0500

    Fixed import bug in models.py.

--- boundaryservice/models.py
+++ boundaryservice/models.py
@@ -1,17 +1,9 @@
-from django.conf import settings
+import re
+
 from django.contrib.gis.db import models
 
 from boundaryservice.fields import ListField, JSONField
 from boundaryservice.utils import get_site_url_root
-
-def get_site_url_root():
-    domain = getattr(settings, 'MY_SITE_DOMAIN', 'localhost')
-    protocol = getattr(settings, 'MY_SITE_PROTOCOL', 'http')
-    port     = getattr(settings, 'MY_SITE_PORT', '')
-    url = '%s://%s' % (protocol, domain)
-    if port:
-        url += ':%s' % port
-    return url
 
 class SluggedModel(models.Model):
     """
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name = "django-boundaryservice",
-    version = "0.1.3",
+    version = "0.1.4",
     description = "A reusable system for aggregating and providing API access to regional boundary data.",
     long_description = 'See `django-boundaryservice <https://github.com/newsapps/django-boundaryservice>`_ on Github for more information.',
     author='Christopher Groskopf',
