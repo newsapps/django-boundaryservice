803731798dea4001f3923821892717835ed92999
Sean Stoops <brutimus@gmail.com>
2019-10-13 13:24:37 -0700

    Upgrading urls file for new Django.

--- boundaryservice/urls.py
+++ boundaryservice/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include 
+from django.conf.urls import include, url
 from tastypie.api import Api
 
 from boundaryservice.resources import BoundarySetResource, BoundaryResource
@@ -8,7 +8,7 @@ v1_api = Api(api_name='1.0')
 v1_api.register(BoundarySetResource())
 v1_api.register(BoundaryResource())
 
-urlpatterns = patterns('',
-    (r'^(?P<api_name>1.0)/(?P<resource_name>boundary-set)/(?P<slug>[\w\d_.-]+)/(?P<external_id>[\w\d_.-]+)$', external_id_redirects),
-    (r'', include(v1_api.urls)),
-)
+urlpatterns = [
+    url(r'^(?P<api_name>1.0)/(?P<resource_name>boundary-set)/(?P<slug>[\w\d_.-]+)/(?P<external_id>[\w\d_.-]+)$', external_id_redirects),
+    url(r'', include(v1_api.urls)),
+]
